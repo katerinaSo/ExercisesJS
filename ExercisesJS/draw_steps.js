85f03c75e47368e1fdea294f39a57ca2ae6bc04a
KaterinaSo <katya.solstad@gmail.com>
2019-02-12 11:32:06 -0500

    function using iteration for drawing sort of stairway looking thing in a console

---
+++ ExercisesJS/draw_steps.js
@@ -0,0 +1,16 @@
+function draw_steps(n) {
+// n lines with n characters each line
+// returning drawing of lines in a console
+// return array of strings representing each line for steps
+  steps = [];
+  for (let line = 0; line < n; line++) {
+    steps[line] = [];
+    for (let place = 0; place < n; place++) {
+      place<=line ? (steps[line][place] = "#") : (steps[line][place] = " ");
+    }
+    steps[line] = steps[line].join("");
+    console.log(steps[line]);
+  }
+  return steps
+}
+draw_steps(3);
