c801afe0f7cd77aa3d742fac4c325dec6246733c
KaterinaSo <katya.solstad@gmail.com>
2019-02-12 14:06:25 -0500

    cleaned up by taking out console.log examples that had there for testing

--- ExercisesJS/reverse_string.js
+++ ExercisesJS/reverse_string.js
@@ -1,9 +1,9 @@
 function reverse_string(str) {
   const string = str
-  .split("")
-  .reverse()
-  .join("");
-  return string ;
+    .split("")
+    .reverse()
+    .join("");
+  return string;
 }
 
 reverse_string("hen");
