aace9b82a2d45da6c4d25280c4e0cce3d7eabd18
KaterinaSo <katya.solstad@gmail.com>
2019-02-13 15:02:28 -0500

    added fibonacci solution with optimized tail call that calculates fast to the last number that computer could show. at 1478 it started to show infinity)

---
+++ ExercisesJS/fib_recursive.js
@@ -0,0 +1,11 @@
+function fib(arr, n) {
+  arr[0] = 0;
+  arr[1] = 1;
+  if (arr.length >= n) {
+    return arr;
+  }
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return fib(arr, n);
+}
+
+console.log(fib([],1478))
